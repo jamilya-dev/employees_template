6da4508744ba54272b16a51ccd391e201926d29c
jamilya-dev <omg.groups@yandex.ru>
2024-11-01 14:51:26 +0300

    submit form

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+import nextId from 'react-id-generator';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,25 +8,53 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
-  const data = [
-    { name: 'John C.', salary: 800, increase: false, id: 1 },
-    { name: 'Alex M.', salary: 3000, increase: true, id: 2 },
-    { name: 'Carl W.', salary: 5000, increase: false, id: 3 },
-  ];
-  return (
-    <div className="app">
-      <AppInfo />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: 'John C.', salary: 800, increase: false, id: 1 },
+        { name: 'Alex M.', salary: 3000, increase: true, id: 2 },
+        { name: 'Carl W.', salary: 5000, increase: false, id: 3 },
+      ],
+    };
+    this.maxId = 4;
+  }
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return {
+        data: data.filter((item) => item.id !== id),
+      };
+    });
+  };
+  addItem = (event, name, salary) => {
+    event.preventDefault();
+    const htmlId = nextId();
+    const newObject = {
+      name: name,
+      salary: salary,
+      increase: false,
+      id: htmlId,
+    };
 
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+    const updatedData = [...this.state.data, newObject];
+
+    this.setState({ data: updatedData });
+  };
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+        <EmployeesList data={this.state.data} onDelete={this.deleteItem} />
+        <EmployeesAddForm data={this.state.data} onAdd={this.addItem} />
       </div>
-
-      <EmployeesList data={data} />
-      <EmployeesAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,23 +1,36 @@
+import { Component } from 'react';
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
+class EmployeesAddForm extends Component {
+  constructor(prors) {
+    super(prors);
+    this.state = {
+      name: '',
+      salary: '',
+    };
+  }
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  render() {
+    const { name, salary } = this.state;
+    const { onAdd } = this.props;
     return (
-        <div className="app-add-form">
-            <h3>Добавьте нового сотрудника</h3>
-            <form
-                className="add-form d-flex">
-                <input type="text"
-                    className="form-control new-post-label"
-                    placeholder="Как его зовут?" />
-                <input type="number"
-                    className="form-control new-post-label"
-                    placeholder="З/П в $?" />
+      <div className="app-add-form">
+        <h3>Добавьте нового сотрудника</h3>
+        <form className="add-form d-flex" onSubmit={(event) => onAdd(event, name, salary)}>
+          <input type="text" className="form-control new-post-label" placeholder="Как его зовут?" name="name" value={name} onChange={this.onValueChange} />
+          <input type="number" className="form-control new-post-label" placeholder="З/П в $?" name="salary" value={salary} onChange={this.onValueChange} />
 
-                <button type="submit"
-                        className="btn btn-outline-light">Добавить</button>
-            </form>
-        </div>
-    )
+          <button type="submit" className="btn btn-outline-light">
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default EmployeesAddForm;
