6684dbb57dedcd32e4d11bda1b3125f4f15f5117
jamilya-dev <omg.groups@yandex.ru>
2024-11-02 15:14:52 +0300

    filter and search

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,6 +17,8 @@ class App extends Component {
         { name: 'Alex M.', salary: 3000, like: false, increase: true, id: 2 },
         { name: 'Carl W.', salary: 5000, like: false, increase: false, id: 3 },
       ],
+      term: '',
+      filter: '',
     };
   }
   deleteItem = (id) => {
@@ -52,18 +54,44 @@ class App extends Component {
       }),
     }));
   };
+  searchEmp = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+    return items.filter((item) => {
+      return item.name.indexOf(term) > -1;
+    });
+  };
+  onUpdateSearch = (term) => {
+    this.setState({ term });
+  };
+  filterPost = (items, filter) => {
+    switch (filter) {
+      case 'like':
+        return items.filter((item) => item.like);
+      case 'moreThen1000':
+        return items.filter((item) => item.salary > 1000);
+      default:
+        return items;
+    }
+  };
+  onFilterSelect = (filter) => {
+    this.setState({ filter });
+  };
   render() {
+    const { data, term, filter } = this.state;
     const employees = this.state.data.length;
     const increased = this.state.data.filter((item) => item.increase).length;
+    const visibleData = this.filterPost(this.searchEmp(data, term), filter);
     return (
       <div className="app">
         <AppInfo employees={employees} increased={increased} />
 
         <div className="search-panel">
-          <SearchPanel />
-          <AppFilter />
+          <SearchPanel onUpdateSearch={this.onUpdateSearch} />
+          <AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
         </div>
-        <EmployeesList data={this.state.data} onDelete={this.deleteItem} onToggleProp={this.onToggleProp} />
+        <EmployeesList data={visibleData} onDelete={this.deleteItem} onToggleProp={this.onToggleProp} />
         <EmployeesAddForm onAdd={this.addItem} />
       </div>
     );
